bd28e64179f6a1e0a7fcfa51801031178a87e26a
Jamil Yanez <jamilyanezvasquez@gmail.com>
2025-05-19 11:20:44 -0500

    Setup Clase cliente y menu

---
+++ cliente.py
@@ -0,0 +1,4 @@
+class Cliente(self, name, id):
+    self.name = name 
+    self.id = id
+    self.pais = "EC"
---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    print("1. Agregar Cliente")
+    print("2. Eliminar Cliente")
+    print("3. Listar Clientes")
+    print("4. Buscar cliente por Nombre")
+    print("5. Determinar Pais del Cliente")
+    print("6. Salir")
+
+    eleccion = input("\nElija una opcion: ")
+
+
+
+if __name__ == "__main__":
+    main()
